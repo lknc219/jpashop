c0a6699964e17ab9f0553dee8fa7a671d085471e
WorkManNamChul <knc219@naver.com>
2021-06-09 17:55:04 +0900

    20210609 Service, Repository 인잭션

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // final이 있는 필드만 가지고 생성자를 만들어줌.
+//@AllArgsConstructor //필드를 가지고 알아서 생성자를 만들어줌
+public class MemberService {
+
+
+    private final MemberRepository memberRepository;
+
+    //생성자가 하나밖에없으면 자동으로 스프링이 주입해준다.
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    @Transactional
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); //중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long id) {
+        return memberRepository.findOnd(id);
+    }
+
+}
--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -17,26 +17,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberRepositoryTest {
 
     @Autowired
-    MemberRepository memberRepository;
+ //   MemberRepository memberRepository;
 
     @Test
     @Transactional
     @Rollback(value = false)
     public void testMember() throws Exception{
         //given
-        Member member = new Member();
-        member.setUsername("member1");
-        
+        //Member member = new Member();
+        //member.setUsername("member1");
+
         //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
+        //Long savedId = memberRepository.save(member);
+        //Member findMember = memberRepository.find(savedId);
 
         //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
+        //Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        //Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        //Assertions.assertThat(findMember).isEqualTo(member);
 
-        System.out.println("(findMember == member) = " + (findMember == member));
+        //System.out.println("(findMember == member) = " + (findMember == member));
 
     }
 }
