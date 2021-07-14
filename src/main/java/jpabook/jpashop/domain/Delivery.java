6bb821296fa32308b6939826975aa42260e486ff
namChul <knc219@naver.com>
2021-07-14 17:59:13 +0900

    20210714 v2 메모하고 수업진행하기

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,8 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-        @OneToOne(mappedBy = "delivery",fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "delivery",fetch = FetchType.LAZY)
+    @JsonIgnore
     private Order order;
 
     @Embedded
