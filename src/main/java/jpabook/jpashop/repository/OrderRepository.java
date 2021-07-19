7064c708776cb7e12a2c249710977d3b5f5be6e5
namChul <knc219@naver.com>
2021-07-19 17:58:01 +0900

    20210719

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -102,6 +102,15 @@ public class OrderRepository {
         return resultList;
     }
 
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o"+
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
 }
 
 
